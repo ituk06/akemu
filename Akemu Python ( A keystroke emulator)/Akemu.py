3e83eef9b2d7f2da15ae34ffa25c3fc2886371ac
Rashid-Yusubov <123075706+Rashid-Yusubov@users.noreply.github.com>
2024-02-11 04:36:57 +0300

    Update Akemu.py
    - Добавлен интерфейс
    - Добавлен таймер

--- Akemu Python ( A keystroke emulator)/Akemu.py
+++ Akemu Python ( A keystroke emulator)/Akemu.py
@@ -1,16 +1,82 @@
-import pyperclip #pyperclip для работы с буфером обмена 
-import keyboard #keyboard для эмуляции нажатий клавиш
-import time #time для работы со временем 
-import random #random для генерации случайных чисел.
-def type_text(buffer): 
-    for character in buffer: 
-        if keyboard.is_pressed('p'): 
-            print('Программа остановлена') 
-            break 
-        keyboard.write(character, delay = random.uniform(0.095, 0.15)) #
-text = pyperclip.paste() 
-print('Ваш скопированный текст:') 
-print(text) 
-time.sleep(5) 
-buffer = pyperclip.paste() 
-type_text(buffer) 
+import pyperclip
+import keyboard
+import random
+import customtkinter
+
+
+class AkemuPython(customtkinter.CTk):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        self.title("Akemu")
+        self.geometry("355x380")
+        self.grid_columnconfigure(0, weight=1)
+        self.resizable(width=False, height=False)
+        self.attributes('-topmost', 1)
+
+        logo_label = customtkinter.CTkLabel(self, text="Буфер обмена",
+                                            font=customtkinter.CTkFont(size=20, weight="bold"))
+        logo_label.grid(row=0, column=0, padx=20, pady=(20, 0))
+        self.countdown_label = customtkinter.CTkLabel(self, text="00:10",
+                                                      font=customtkinter.CTkFont(size=20, weight="bold"))
+        self.countdown_label.grid(row=0, column=1, padx=(95, 20), pady=(20, 0))
+        self.textbox = customtkinter.CTkTextbox(self, width=300, height=240)
+        self.textbox.grid(row=1, column=0, padx=20, pady=(20, 0), sticky="nsew", columnspan=2)
+        self.textbox.insert("0.0", pyperclip.paste())
+        self.textbox.configure(state='disabled')
+        button = customtkinter.CTkButton(self, text="Начать", command=self.execute_commands)
+        button.grid(row=2, column=0, padx=20, pady=20, sticky="ew", columnspan=2)
+
+        self.clipboard_content = pyperclip.paste()
+        self.check_clipboard()
+
+    def execute_commands(self):
+        self.start_countdown()
+        self.after(11000, self.start_typing)
+
+    def start_countdown(self):
+        self.countdown(10)
+
+    def countdown(self, count):
+        if count >= 0:
+            mins, secs = divmod(count, 60)
+            timer = '{:02d}:{:02d}'.format(mins, secs)
+            self.countdown_label.configure(text=timer)
+            self.after(1000, self.countdown, count - 1)
+        else:
+            self.countdown_label.configure(text="00:10")
+
+    @staticmethod
+    def type_text(buffer):
+        for character in buffer:
+            if keyboard.is_pressed('p'):
+                print('Программа остановлена')
+                break
+            keyboard.write(character, delay=random.uniform(0.095, 0.15))
+
+    def start_typing(self):
+        text = pyperclip.paste()
+        print('Ваш скопированный текст:')
+        print(text)
+        buffer = pyperclip.paste()
+        self.type_text(buffer)
+
+    def check_clipboard(self):
+        new_clipboard_content = pyperclip.paste()
+        if new_clipboard_content != self.clipboard_content:
+            self.clipboard_content = new_clipboard_content
+            self.update_textbox()
+        self.after(1000, self.check_clipboard)
+
+    def update_textbox(self):
+        current_position = self.textbox.index('insert')
+        self.textbox.configure(state='normal')
+        self.textbox.delete('1.0', 'end')
+        self.textbox.insert('1.0', self.clipboard_content)
+        self.textbox.configure(state='disabled')
+        self.textbox.see(current_position)
+
+
+if __name__ == "__main__":
+    app = AkemuPython()
+    app.mainloop()
